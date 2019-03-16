c14923d277299877ffb7b203cb0ab3ca8932f59e
vlad0696 <vladislav-gramovich@mail.ru>
2019-03-16 18:56:37 +0300

    it's ok

---
+++ DebtorsSystem/Models/DebtorModel.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DebtorsSystem.Models
+{
+    public class DebtorModel
+    {
+        public string Id { get; set; }
+        public string FIO { get; set; }
+        public string DateIssue { get; set; }
+        public string TrainingLevel { get; set; }
+        public string Address { get; set; }
+        public string DateWorkStarted { get; set; }
+        public string DateWorkStopped { get; set; }
+        public string RefundAmount { get; set; }
+        public string DateRefund { get; set; }
+        public string RefundBeforeTrial { get; set; }
+        public string DateTrial { get; set; }
+        public string RefundAfterTrial { get; set; }
+        public string ExecutionStopped { get; set; }
+        public string DateExecution { get; set; }
+        public string DateResumptionExecution { get; set; }
+        public string RefundResidue { get; set; }
+        public string Reimbursed { get; set; }
+        public string Mails { get; set; }
+
+        public Debtor ConvertToDepter(ref Debtor debtor)
+        {
+            debtor.Id = int.Parse(Id);
+            debtor.FIO = FIO;
+            debtor.DateIssue = convertToDate(DateIssue);
+            if (TrainingLevel == "1")
+            {
+                debtor.TrainingLevel = "ПТО";
+            }
+            else
+            {
+                debtor.TrainingLevel = "ССО";
+            }
+            debtor.Address = Address;
+            debtor.DateWorkStarted= DateWorkStarted != "" ? convertToDate(DateWorkStarted) : new DateTime(1, 1, 1);
+            debtor.DateWorkStopped = DateWorkStopped != "" ? convertToDate(DateWorkStopped) : new DateTime(1, 1, 1);
+            debtor.RefundAmount = RefundAmount;
+            debtor.DateRefund = DateRefund!= "" ? convertToDate(DateRefund):new DateTime(1,1,1);
+            debtor.RefundBeforeTrial = RefundBeforeTrial;
+            debtor.DateTrial = DateTrial != "" ? convertToDate(DateTrial) : new DateTime(1, 1, 1);
+            debtor.RefundAfterTrial = RefundAfterTrial;
+            debtor.DateExecution = DateExecution != "" ? convertToDate(DateExecution) : new DateTime(1, 1, 1);
+            debtor.DateResumptionExecution = DateExecution != "" ? convertToDate(DateResumptionExecution) : new DateTime(1, 1, 1);
+            debtor.Mails = Mails;
+            return debtor;
+        }
+
+        public Debtor ConvertToDepter()
+        {
+            Debtor debtor = new Debtor();
+            debtor.FIO = FIO;
+            debtor.DateIssue = convertToDate(DateIssue);
+            if (TrainingLevel == "1")
+            {
+                debtor.TrainingLevel = "ПТО";
+            }
+            else
+            {
+                debtor.TrainingLevel = "ССО";
+            }
+            debtor.Address = Address;
+            debtor.DateWorkStarted = DateWorkStarted != "" ? convertToDate(DateWorkStarted) : new DateTime(1, 1, 1);
+            debtor.DateWorkStopped = DateWorkStopped != "" ? convertToDate(DateWorkStopped) : new DateTime(1, 1, 1);
+            debtor.RefundAmount = RefundAmount;
+            debtor.DateRefund = DateRefund != "" ? convertToDate(DateRefund) : new DateTime(1, 1, 1);
+            debtor.RefundBeforeTrial = RefundBeforeTrial;
+            debtor.DateTrial = DateTrial != "" ? convertToDate(DateTrial) : new DateTime(1, 1, 1);
+            debtor.RefundAfterTrial = RefundAfterTrial;
+            debtor.DateExecution = DateExecution != "" ? convertToDate(DateExecution) : new DateTime(1, 1, 1);
+            debtor.DateResumptionExecution = DateExecution != "" ? convertToDate(DateResumptionExecution) : new DateTime(1, 1, 1);
+            debtor.Mails = Mails;
+            return debtor;
+        }
+        //Февраль 20, 2019
+        private DateTime convertToDate(string value)
+        {
+            
+            string[] values = value.Split(' ');
+            values[1] = values[1].Remove(values[1].Length-1, 1);
+            string[] months = { "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь" };
+            int month = 0;
+            for(int i = 0; i < 12; i++)
+            {
+                if (months[i] == values[0])
+                {
+                    month = i + 1;
+                }
+            }
+            DateTime dateTime = new DateTime(int.Parse(values[2]), month, int.Parse(values[1]));
+            return dateTime;
+        }
+    }
+
+}
---
+++ DebtorsSystem/Startup.cs
@@ -0,0 +1,68 @@
+﻿using System.IO;
+using DebtorsSystem.Models;
+using DebtorsSystem.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace DebtorsSystem
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            string connection = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<DebtorContext>(options => options.UseSqlServer(connection));
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddHostedService<NotificationService>();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Debtor/Error");
+                app.UseHsts();
+            }
+            var host = new WebHostBuilder()
+                        .UseKestrel()
+                        .UseContentRoot(Directory.GetCurrentDirectory())
+                        .UseIISIntegration()
+                        .UseStartup<Startup>()
+                        .Build();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+            app.UseDeveloperExceptionPage();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Debtor}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
