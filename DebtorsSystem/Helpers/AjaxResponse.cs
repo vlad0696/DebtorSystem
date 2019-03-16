bb5d12224a42b7e1d559914db2caa07e63d61981
vlad0696 <vladislav-gramovich@mail.ru>
2019-03-16 18:52:21 +0300

    restore

---
+++ DebtorsSystem/Helpers/AjaxResponse.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DebtorsSystem.Helpers
+{
+    public class AjaxResponse
+    {
+        public AjaxResponse()
+        {
+            Success = true;
+            Data = new List<object>();
+        }
+
+        public AjaxResponse(Exception exception)
+            : this()
+        {
+            Success = false;
+            Errors = new[] { exception.Message };
+        }
+
+        public AjaxResponse(object data)
+            : this()
+        {
+            Data = data;
+        }
+
+        public bool Success
+        {
+            get; set;
+        }
+        public object Data
+        {
+            get; set;
+        }
+        public string[] Errors
+        {
+            get; set;
+        }
+    }
+}
---
+++ DebtorsSystem/Models/DebtorContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DebtorsSystem.Models
+{
+    public class DebtorContext:DbContext
+    {
+        public DbSet<Debtor> Debtors { get; set; }
+
+        public DebtorContext(DbContextOptions<DebtorContext> options)
+           : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
